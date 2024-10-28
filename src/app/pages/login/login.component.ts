09f51aa51e50b4403001d39007f2ac9e9dcd5936
Nguyễn Trần Nam Khánh <khanh.ntn@mor.com.vn>
2024-10-28 11:46:41 +0700

    Feat: validation for login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,31 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '@services/auth/auth.service';
+import { EmailValidatorDirective } from 'app/directives/email-validator.directive';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, EmailValidatorDirective],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
   email: string = '';
   password: string = '';
   errorMessage: string = '';
+  submitted = false;
+  loading = false;
+  @ViewChild('myForm') form!: NgForm;
 
   constructor(private authService: AuthService, private router: Router) {}
 
   login() {
-    this.authService.login(this.email, this.password).subscribe({
-      next: () => {
-        this.router.navigate(['/todo-list']); 
-      },
-      error: (err) => {
-        console.error('Login failed', err);
-      }
-    });
+    this.submitted = true;
+    this.loading = true;
+
+    if (this.form.valid) {
+      this.authService.login(this.form.value.email, this.form.value.password).subscribe({
+        next: () => {
+          this.router.navigate(['/todo-list']);
+        },
+        error: (err) => {
+          console.error('Login failed', err);
+          this.errorMessage = 'Login failed. Please check your credentials.';
+        },
+        complete: () => {
+          this.loading = false; 
+        },
+      });
+    } else {
+      this.loading = false; 
+      this.errorMessage = 'Please fill out the form correctly.';
+    }
   }
 }
